150c6aaa5892aaabdd95ebc651a3052075226512
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-09 15:11:31 -0700

    A commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	int str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	putchar(str);
 	return (1);
 }

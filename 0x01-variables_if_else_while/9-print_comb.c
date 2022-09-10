8a7515843cae936652a5d56131b6d0c50e6a9eaa
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-09 19:14:55 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 	int x;
-	
+
 	for (x = 48; x < 58; x++)
 	{
-		putchar(x);
-		if(x != 57)
+		putchar(i);
+		if (x != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	 putchar('\n');
-	 return (0);
+	putchar('\n');
+	return (0);
 }

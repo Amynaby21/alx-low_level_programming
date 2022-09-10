c35adf04836b5d640ff5aecca63c8ecc1cf4c1cb
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-09 17:44:58 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 20; i < 26; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}

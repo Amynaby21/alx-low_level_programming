7d3720328f11821d4ca6390b25a4022005f424dc
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-14 15:38:36 -0700

    An absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Checks for the absolute value of an interger
+ *
+ * @i: The integer to be checked
+ *
+ * Return: The absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}

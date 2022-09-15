4e79b3ce946faa5f35abd1ddb9e83bfb4facfe76
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-14 17:30:48 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ *@n1: Numbers added
+ *@n2: Numbers added
+ *
+ * Return: Always 0
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

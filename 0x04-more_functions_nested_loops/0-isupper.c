0e74cef3ebb867f351c8398cd6d4ce9faba40dfa
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-16 09:28:33 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for a uppercase character
+ * @c: alphabet letter tested
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

267b067a26a807d8d98c331ea9b53d13eaeb0bad
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-16 11:25:52 -0700

    Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** print_diagonal - Draws a diagonal line
+ * @n: The nimbers of times the character \ is prited
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			if (i < (n - 1))
+				_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}

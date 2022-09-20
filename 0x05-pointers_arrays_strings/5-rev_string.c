714b10a2ba38bd39c10eafb37c94a65faa1b7acc
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-19 17:22:56 -0700

    The reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Prints the reverse of a string
+ * @s: The string that is reversed
+ */
+
+void rev_string(char *s)
+{
+	int i, len, temp;
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}

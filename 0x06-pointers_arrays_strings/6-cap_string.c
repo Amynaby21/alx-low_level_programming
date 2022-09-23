b8e9fe79c62f2fb79b06b0d6f223c07a83cdb03d
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-09-23 12:35:22 -0700

    Capitalize a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,33 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all word of a string
+ * cap_string - Capitalizes all words of a string
  *
- *@str: The string to capitalize
+ *@str: The string
  *
- *Return: str
  */
 
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0, j;
+	int separator[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 41, 40, 123, 125};
 
+	if (str[i] >= 97 && str[i] <= 122)
+	{
+		str[i] = str[i] - 32;
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0)
+		for (j = 0; j < 13; j++)
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
-			continue;
-		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i] == separator[j])
 			{
-				str[i] = str[i] - 32;
-				continue;
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] = str[i + 1] - 32;
+					break;
+				}
 			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] - 32;
 		}
 	}
 	return (str);

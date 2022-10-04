b2d0918cbe20e6a94e6d33dea749fb82ed112681
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-04 11:05:36 -0700

    Create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,10 +20,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	a = malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
-		a[i] = c;
 	if (a == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		a[i] = c;
 
 	return (a);
 }

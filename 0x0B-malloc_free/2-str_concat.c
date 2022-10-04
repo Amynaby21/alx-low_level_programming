e351dc20b85c4af05e2b139b01d4306d402de092
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-04 14:06:37 -0700

    Concate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,9 +22,9 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		n++;
-	for (i = 0; s2[i] !='\0'; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 		n++;
-	str = malloc(sizeof(char) * n);
+	str = malloc(sizeof(char) * (n + 1));
 
 	if (str == NULL)
 		return (NULL);

447e8156bf94b05c1acc9c4a4bc4f2c163647487
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-07 12:43:41 -0700

    String concate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  *Return: The concatenate string
  */
 
-char *str_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int i;

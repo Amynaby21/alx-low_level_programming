a947423539809daf59c4545315a69114eda8d9e3
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-07 12:48:09 -0700

    String concate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,11 +25,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (str == NULL)
 		return (NULL);
+	n = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
 		str[n++] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++)
 		str[n++] = s2[i];
+
+	str[n] = '\0';
 
 	return (str);
 }

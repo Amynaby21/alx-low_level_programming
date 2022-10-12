75d1e1a7f66a6200ebb4426157e5b9aaf774c653
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-12 07:35:58 -0700

    Prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name
+ *
+ * @name: The name to print
+ * @f: The function pointed
+ *
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	if (name || f)
+		f(name);
+}

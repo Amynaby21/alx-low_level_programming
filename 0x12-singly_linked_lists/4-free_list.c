6fed53b6f16ae8d1519d4f173cb15fb92ea79aaf
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-20 16:12:07 -0700

    Frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ *
+ * @head: A pointer to the first node
+ */
+
+void free_list(list_t *head)
+{
+	list_t current;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		current->next = next;
+		free(current);
+		current = next;
+	}
+}

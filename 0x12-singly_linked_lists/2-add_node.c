f0ccaaa9ff6d3f162341d5d91c2fb182a7091bef
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-20 15:33:25 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list
@@ -25,7 +26,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(newnOde);
 		return (NULL);
 	}
-	for (len = 0, str[len]; len++)
+	for (len = 0; str[len]; len++)
 		;
 
 	newnOde->len = len;

48ddce1c91b5a6fee9a1ef37f85c1c4de36c3621
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-20 15:31:05 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,21 +13,23 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnOde;
+	char *sdup;
 	int len;
 
 	newnOde = malloc(sizeof(list_t));
 	if (newnOde == NULL)
 		return (NULL);
-	newnOde->str = strdup(str);
-	if (strdup(str) == NULL)
+	sdup = strdup(str);
+	if (sdup == NULL)
 	{
 		free(newnOde);
 		return (NULL);
 	}
 	for (len = 0, str[len]; len++)
 		;
+
 	newnOde->len = len;
-	newnOde->str = strdup(str);
+	newnOde->str = sdup;
 	newnOde->next = *head;
 	*head = newnOde;
 	return (newnOde);

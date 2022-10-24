9f3121458ffad123e8a9408f3f8ec5b2bab6d1db
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-24 14:04:22 -0700

    Prints linstint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list
+ * @h: The linked list
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}

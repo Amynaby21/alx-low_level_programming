e844b8a9372f231ba293186169c4deb1f12c5102
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-24 15:04:12 -0700

    List len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a linked list
+ * @h: The linked list
+ * Return: The number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t elm = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elm++;
+	}
+	return (elm);
+}

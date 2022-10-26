976185b6e1ac1626819c07755138034b5d3a3a06
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-26 12:16:35 -0700

    Frees list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,10 +6,12 @@
 
 void free_listint(listint_t *head)
 {
+	listint_t *temp;
+
 	while (head != NULL)
 	{
-		free(head->next);
+		temp = head->next;
 		free(head);
-		head = head->next;
+		head = temp;
 	}
 }

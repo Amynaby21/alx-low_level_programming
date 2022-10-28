eb87c7fccc722414c15b0b76862c117b8006b88a
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-10-28 11:57:28 -0700

    Gets bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int num;
 
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+
 	num = n >> (index - 1);
 	return (num & 1);
 }

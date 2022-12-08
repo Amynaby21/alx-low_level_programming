550432adef7de90c96471033230ab849cb10d45c
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-12-08 14:39:27 -0800

    Node at beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,15 +12,23 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *n_node;
 
-	if (n_node == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	n_node = malloc(sizeof(dlistint_t));
+	if (n_node == NULL)
+		return (NULL);
 	n_node->n = n;
+	if (*head == NULL)
+	{
+		*head = n_node;
+		n_node->prev = NULL;
+		n_node->next = NULL;
+		return (n_node);
+	}
+	n_node->next = (*head);
 	n_node->prev = NULL;
-	n_node->next = *head;
-	if ((*head) != NULL)
-		(*head)->prev = n_node;
+	(*head)->prev = n_node;
 	(*head) = n_node;
 	return (n_node);
 }
